0e91c94340eb87e7efac994c5cb1d3974d7fd9a1
Josh Fisher <jpf200124@gmail.com>
2012-01-27 16:19:15 -0500

    -Initial Commit of Codebase and first CAM.js distributable.

---
+++ README.md
@@ -0,0 +1,83 @@
+﻿# CAM.js #
+
+Client Asset Manager (CAM) is a javascript library for managing assets on a users machine.
+
+The library is designed to help with the growing problem of managing client side assets in HTML5 applications. The library provides all of the code necessary to setup a dynamic asset manager that controls downloading and updating of all assets associated with your HTML5 application. 
+
+The library is cross-browser compatible and has failbacks via xhr for browsers that do not support the FileSystemAPI. 
+
+
+## Usage ##
+
+Below are usage examples for a variety of operations.
+
+#### Asset Configuration ####
+The asset configuration file is a json file that depicts your remote directory structure. You must specify "binary" for the library to correctly write to the FileSystem.
+
+``` json
+
+{
+	"images": {
+		"Files": [
+				{"Filename": "GitHub.svg", "binary": true},
+				{"Filename": "octocat.png", "binary": true}
+			],
+		"mydir1": {
+		},
+		"mydir2": {
+		}
+}
+```
+
+#### Asset Versioning & Updates ####
+The version file is a json  file that tells the library if the clients files need to be update. The file is loaded at initialization and checked against the user's local copy. If updates are needed the process of downloading the files will automatically begin. In order to push updates to your clients on the next page load you will need to update the version file on your server.
+
+``` json
+
+{
+	"version": "1.0",
+	"changes": ""
+}
+```
+
+#### Initialization ####
+
+
+``` javascript
+
+	//Define the Filesystem data
+	FU.LocalRoot = "CAMExample";
+	FU.PersistantSize = 157286400;
+	
+	//Define Configuration File
+	CAM.AssetConfig = "assets.json";
+	CAM.VersionAsset = "version.json";
+	
+	//Define Asset Paths
+	CAM.LocalRootPath = "/assets/";
+	CAM.RemoteRootPath = "assets/";
+
+	//Initialize with callbacks
+	CAM.Initialize(initialized, assetsLoaded);
+```
+
+#### Loading an Asset ####
+In the loading function below the return type can be a FileSystemAPI fileEntry, a FileAPI file, a FileSystemAPI Url, or binary or text data depending upon the fileType.
+
+``` javascript
+
+	CAM.Load({
+		assetName: "octocat.png",
+		returnType: "url",
+		fileType: FU.FILE_TYPE.BINARY, 
+		callback: function(url){
+			document.write("</br></br><img src=" + url +" />");
+			document.write("</br></br>If you are seeing the github Octocat above the example was successfull!");
+		}
+	});
+```
+
+
+## Dependencies ##
+
+[FU.js](https://github.com/jpf200124/FU.js "File Utilities for javascript.") - File Utilities for javascript.
---
+++ src/CAM.js
@@ -0,0 +1,297 @@
+/**
+ * @author JFisher
+ */
+
+/**
+ * @name CAM
+ */
+(function( CAM, undefined ) {
+	
+	if(!window.FU)
+		throw Error("Required library FU.js is not loaded. Aborting ...");
+	
+	CAM.Assets = [];
+	CAM.Directories = [];
+
+	
+	/**********************************************************************************************************************
+	 * Initialization
+	 **********************************************************************************************************************/
+	CAM.Initialize = function(initializedCallback, assetsLoadedCallback){
+		if(FU.CLIENT_FEATURES.FilesystemAPI)
+		{
+			//Init the local Filesystem
+			FU.InitLocalFileSystem(function initialized(){				
+				//Load Remote Assets
+				if(assetsLoadedCallback){
+					initializedCallback();
+					LoadAssets(assetsLoadedCallback);
+				}
+				else{
+					LoadAssets(initializedCallback);
+				}
+			});
+		}else{
+			initializedCallback();
+			
+			//Load Remote Asset Configuration File (Json file)
+			FU.LoadFile({
+				fileType: FU.FILE_TYPE.TXT,
+				loadMechanism: FU.LOAD_MECHANISM.XHR,
+				remoteURL: CAM.RemoteRootPath.concat(CAM.AssetConfig),
+				loaded: function remoteAssetDataLoaded(data){
+					//Parse configuration file into object using json parser
+					var parsedAssetObject = JSON.parse(data);
+					
+					//Parse asset object into directories and asset paths
+					ParseDirectoriesAndAssets(parsedAssetObject, "");
+					
+					assetsLoadedCallback();
+				}
+			});
+		}
+	};
+
+	
+	function LoadAssets(assetsLoadedCallback){
+		//Create the local root path.
+		FU.CreateDirectory(CAM.LocalRootPath, false, function(){});
+		
+
+		//Load Remote Version asset (Json file)
+		FU.LoadFile({
+			fileType: FU.FILE_TYPE.TXT,
+			loadMechanism: FU.LOAD_MECHANISM.XHR,
+			remoteURL: CAM.RemoteRootPath.concat(CAM.VersionAsset),
+			loaded: function(remoteData){
+				//Parse
+				var parsedRemoteVersionAsset = JSON.parse(remoteData);
+				
+				//Check if the local file exists.
+				FU.CheckFileSystemForFile(CAM.LocalRootPath.concat(CAM.VersionAsset), false, function(fileEntry){
+					if(fileEntry !== undefined){
+						//Load Local Version asset (json file)
+						FU.LoadFile({
+							fileType: FU.FILE_TYPE.TXT,
+							loadMechanism: FU.LOAD_MECHANISM.FILESYSTEM,
+							file: fileEntry,
+							loaded: function(localData){
+								//Parse
+								var parsedLocalVersionAsset = JSON.parse(localData);
+								
+								//Compare local and remote versions
+								if(parsedLocalVersionAsset.version !== parsedRemoteVersionAsset.version){
+									console.log("Assets have changed ... updating assets.");
+									
+									//Save version file to filesystem.
+									FU.SaveFile({
+										remoteAsset: remoteData,
+										filePath: CAM.LocalRootPath.concat(CAM.VersionAsset),
+										temporary: false,
+										saved: function(url){
+											//TODO: add to some temp variable to track when things are complete for progress.
+											console.log(url + " Saved To local Filesystem");
+										}
+									}); // END SAVE Asset to Local
+									
+									//Attempt to download all assets
+									downloadAssets();
+								}
+								else{
+									console.log("All assets are up to date!")
+									
+									//Load Remote Asset Configuration File (Json file)
+									FU.LoadFile({
+										fileType: FU.FILE_TYPE.TXT,
+										loadMechanism: FU.LOAD_MECHANISM.XHR,
+										remoteURL: CAM.RemoteRootPath.concat(CAM.AssetConfig),
+										loaded: function remoteAssetDataLoaded(data){
+											//Parse configuration file into object using json parser
+											var parsedAssetObject = JSON.parse(data);
+											
+											//Parse asset object into directories and asset paths
+											ParseDirectoriesAndAssets(parsedAssetObject, "");
+											
+											assetsLoadedCallback();
+										}
+									});
+								}
+							}
+						});
+					}else{
+						//Save version file to filesystem.
+						FU.SaveFile({
+							remoteAsset: remoteData,
+							filePath: CAM.LocalRootPath.concat(CAM.VersionAsset),
+							temporary: false,
+							saved: function(url){
+								//TODO: add to some temp variable to track when things are complete for progress.
+								console.log(url + " Saved To local Filesystem");
+							}
+						}); // END SAVE Asset to Local
+						
+						//Attempt to download all assets
+						downloadAssets();
+					}
+				});
+			}
+		});
+		
+		function downloadAssets(){
+			//Load Remote Asset Configuration File (Json file)
+			FU.LoadFile({
+				fileType: FU.FILE_TYPE.TXT,
+				loadMechanism: FU.LOAD_MECHANISM.XHR,
+				remoteURL: CAM.RemoteRootPath.concat(CAM.AssetConfig),
+				loaded: function remoteAssetDataLoaded(data){
+					//Parse configuration file into object using json parser
+					var parsedAssetObject = JSON.parse(data);
+					
+					//Parse asset object into directories and asset paths
+					ParseDirectoriesAndAssets(parsedAssetObject, "");
+					
+					//Create Directories (If they do not already exist - handled by FileSystemAPI)
+					for (var i=0; i < CAM.Directories.length; i++) {
+						//Get directory
+						var directory = CAM.LocalRootPath.concat(CAM.Directories[i]);
+						
+						//Create Directory
+						FU.CreateDirectory(directory, false, function(){
+							//TODO: add to some temp variable to track when things are complete for progress.
+						});
+			        }
+			        
+			        //Save Assets (If they do not already exist - handled by FileSystemAPI)
+					var loadedcount = 0;
+					for (var i=0; i < CAM.Assets.length; i++) {
+						//Get the asset path
+						var asset = CAM.Assets[i];
+						
+						var fileType = (asset.binary) ? (FU.FILE_TYPE.BINARY) : (FU.FILE_TYPE.TXT);
+						LoadAndSaveAsset(fileType, asset.Filename);
+						
+						function LoadAndSaveAsset(fileType, filePath){
+							FU.CheckFileSystemForFile(CAM.LocalRootPath.concat(filePath), false, function(fileEntry){
+								if(fileEntry !== undefined){
+									loadedcount++;
+								
+									console.log("File: " + CAM.LocalRootPath.concat(filePath) + " is already on Local FileSystem");
+									
+									if(loadedcount ===  CAM.Assets.length)
+										assetsLoadedCallback();
+								}else{
+									//Load Remote Asset File (json file)
+									FU.LoadFile({
+										fileType: fileType,
+										loadMechanism: FU.LOAD_MECHANISM.XHR,
+										remoteURL: CAM.RemoteRootPath.concat(filePath),
+										loaded: function remoteAssetLoaded(remoteAsset){
+											//Save File to local filesystem
+											FU.SaveFile({
+												remoteAsset: remoteAsset,
+												filePath: CAM.LocalRootPath.concat(filePath),
+												temporary: false,
+												saved: function(url){
+													loadedcount++;
+												
+													console.log(url + " Saved To local Filesystem");
+													
+													if(loadedcount ===  CAM.Assets.length)
+														assetsLoadedCallback();
+												}
+											}); // END SAVE Asset to Local
+										}
+									}); // END LOAD Remote Asset
+								}
+							});
+						};
+			        }
+				}
+			}); // END Load Remote Config
+		}
+	}
+		
+	
+	
+	/**********************************************************************************************************************
+	 * Utility Methods
+	 **********************************************************************************************************************/
+	
+	function ParseDirectoriesAndAssets(parentAssetObject, parentPath){
+		var path = parentPath;
+		for(var assetProp in parentAssetObject) {
+		    if(parentAssetObject.hasOwnProperty(assetProp)){
+		    	var assetObj = parentAssetObject[assetProp];
+
+		    	if(assetProp !== "Files"){
+		    		//Set Path
+		    		var newPath = path + "/" + assetProp;
+		    	
+		    		//Push Path
+		    		CAM.Directories.push(newPath);
+		    		
+		    		//Recurse
+		    		ParseDirectoriesAndAssets(assetObj, newPath)
+		    	}
+		    	else{
+		    		for (var i=0; i < assetObj.length; i++) {
+		    			var fileName = assetObj[i].Filename;
+		    			var tmpPath = path + "/" + fileName;
+		    			assetObj[i].Filename = tmpPath;
+		    			CAM.Assets.push(assetObj[i]);
+					};
+		    	}
+		    }
+		}
+	};
+	
+	CAM.GetPathFromFilename = function(filename){
+		//TODO: Currently only searches for remote paths.
+		//		Implement ability to search for local path
+		//		and return either a local or remote path.
+		
+		var filepath = undefined;
+		$.each(CAM.Assets, function(index, value) { 
+			var fileNameIndex = -1
+			if(value.Filename.indexOf(filename) !== -1)
+				fileNameIndex = index;
+				
+			if(fileNameIndex >= 0){
+				filepath = CAM.RemoteRootPath.concat(value.Filename);
+				return true;
+			}
+		});
+		
+		return filepath;
+	}
+	
+	CAM.Load = function(params){
+		var assetName, returnType, fileType, callback;
+		assetName = params.assetName;
+		returnType = params.returnType;
+		fileType = params.fileType;
+		callback = params.callback;
+		
+		if(FU.CLIENT_FEATURES.FilesystemAPI){
+			FU.LoadFile({
+		        fileType: fileType,
+		        loadMechanism: FU.LOAD_MECHANISM.FILESYSTEM,
+		        fileName: assetName,
+		        returnType: returnType,
+		        temporary: false,
+		        loaded: callback
+		    });
+		}
+		else{
+			var remoteURL = CAM.GetPathFromFilename(assetName);
+			
+			FU.LoadFile({
+		        fileType: fileType,
+		        loadMechanism: FU.LOAD_MECHANISM.XHR,
+		        remoteURL: remoteURL,
+		        loaded: callback
+		    });
+		}
+	}
+	
+}( window.CAM = window.CAM|| {} ));
